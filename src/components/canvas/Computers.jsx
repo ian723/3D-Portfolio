fd7a846767b6feff6888da25ff01f4f82d92c25c
Ian Ndegwa <ndegwaian241@gmail.com>
2024-11-20 09:33:01 +0300

    message

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf"); // Ensure this path is correct
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
